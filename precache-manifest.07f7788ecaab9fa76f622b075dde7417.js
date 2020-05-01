d7c07fc48eee113762474fe86823f3e45515eda0
Apple <apple@MacBookPro.local>
2020-05-01 18:34:06 +0530

    Updates

---
+++ precache-manifest.07f7788ecaab9fa76f622b075dde7417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54ddee6caa1d4c6b848190fe114e296",
+    "url": "/shopcart/index.html"
+  },
+  {
+    "revision": "a7d9d5cb4dc9ba73fbce",
+    "url": "/shopcart/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5eda92e72fe7a1953cee",
+    "url": "/shopcart/static/js/2.821a2da1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopcart/static/js/2.821a2da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d9d5cb4dc9ba73fbce",
+    "url": "/shopcart/static/js/main.b40cc9ef.chunk.js"
+  },
+  {
+    "revision": "04cdc1f6dbe637185daf",
+    "url": "/shopcart/static/js/runtime-main.9e6abd2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopcart/static/media/logo.5d5d9eef.svg"
+  }
+]);
